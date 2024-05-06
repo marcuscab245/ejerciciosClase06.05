25758b7547f4eddced82cd4fa7f16cae538b8a04
marcuscab245 <164245061+marcuscab245@users.noreply.github.com>
2024-05-06 12:34:59 -0600

    Tabla del 12 06.05.24

---
+++ claseWhile.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int i = 1;
+    int o = 12;
+    int r = 0;
+
+    while (i<=12){
+    r = i * o;
+
+    cout << r << "\n";
+    i++;
+
+    }
+
+    return 0;
+}
